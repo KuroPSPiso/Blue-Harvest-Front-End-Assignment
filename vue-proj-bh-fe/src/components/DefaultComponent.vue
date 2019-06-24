f958a4aa16b17576a82042ca6ca8fdee5debdca6
Ryan van den Bogaard <bogaardryan@hotmail.com>
2019-06-24 07:26:24 +0200

    default-component-template

---
+++ vue-proj-bh-fe/src/components/DefaultComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="default-component">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultComponent',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<style>
+.default-component{
+    
+}
+</style>
